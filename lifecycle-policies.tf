fbb7ebf675fac147780b9c3cfbff2a0ab61189a8
Cristian Beraha <cristian@cloudops.works>
2025-04-17 18:58:37 -0300

    Refactor ECR lifecycle policy to use descriptive rule keys
    Updated the `for_each` constructs to use descriptive keys based on rule priority, improving clarity and preventing potential conflicts. This ensures better maintainability and alignment with Terraform best practices.

--- lifecycle-policies.tf
+++ lifecycle-policies.tf
@@ -7,7 +7,9 @@
 data "aws_ecr_lifecycle_policy_document" "default_lifecycle_policy" {
   count = length(var.default_lifecycle_policy, []) > 0 ? 1 : 0
   dynamic "rule" {
-    for_each = try(var.default_lifecycle_policy.rules, [])
+    for_each = {
+      for rule in try(var.default_lifecycle_policy.rules, []) : "default-${rule.rule_priority}" => rule
+    }
     content {
       rule_priority = rule.value.rule_priority
       description   = try(rule.value.description, null)
@@ -31,7 +33,9 @@ data "aws_ecr_lifecycle_policy_document" "lifecycle_policy" {
     for k, repo in local.repos : k => repo if length(try(repo.lifecycle_policy_rules, [])) > 0
   }
   dynamic "rule" {
-    for_each = try(each.value.lifecycle_policy_rules, [])
+    for_each = {
+      for rule in try(each.value.lifecycle_policy_rules, []) : "${each.k}-${rule.rule_priority}" => rule
+    }
     content {
       rule_priority = rule.value.rule_priority
       description   = try(rule.value.description, null)
